e1302d4c5e1615d4d515733bf86e071ed2fa90e7
olesatanya <olesatanya@gmail.com>
2022-03-10 04:17:32 -0800

    fix:generate

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const generate_images = () => {
     var totalCount = 0;
     const generate = async ({ dirs, combineDatas, resImgPath, index }) => {
         if (dirs[index] == null) {
-            await combineImages({ imageDatas: combineDatas, resImgPath: `${resImgPath}/res_${totalCount}.png` });
+            await combineImages({ imageDatas: combineDatas, resPath: `${resImgPath}/res_${totalCount}.png` });
             totalCount++;
             return;
         }
 
         const length = fs.readdirSync(dirs[index]).length;
         for (var i = 1; i <= length; i++) {
-            await generate({ dirs, combineDatas: combineDatas.concat([`${dirs[index]}/c (${i}).png`]), index: index + 1 })
+            await generate({ dirs, combineDatas: combineDatas.concat([`${dirs[index]}/c (${i}).png`]), index: index + 1, resImgPath })
         }
     }
     generate(initDatas);
@@ -40,4 +40,4 @@ const uploadImages = async () => {
     await upload(initDatas);
 }
 
-uploadImages();
+generate_images();
